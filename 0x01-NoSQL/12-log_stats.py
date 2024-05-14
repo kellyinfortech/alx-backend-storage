e8301cf3e8ff65b5c7c59b7d7613252b3f5bcd19
kellyinfortech <kellyinfortech.solution@gmail.com>
2024-05-14 11:41:38 -0700

    mangodb

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
-"""
-script that provides some stats about Nginx logs stored in MongoDB
+"""Defines a function that  provides some stats
+   about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_logs = client.logs.nginx
-    # get number of documents in collection
-    docs_num = nginx_logs.count_documents({})
-    get_num = nginx_logs.count_documents({'method': 'GET'})
-    post_num = nginx_logs.count_documents({'method': 'POST'})
-    put_num = nginx_logs.count_documents({'method': 'PUT'})
-    patch_num = nginx_logs.count_documents({'method': 'PATCH'})
-    delete_num = nginx_logs.count_documents({'method': 'DELETE'})
-    get_status = nginx_logs.count_documents({'method': 'GET',
-                                             'path': '/status'})
-    print("{} logs".format(docs_num))
+def nginx_stats_check():
+    """provides some stats about Nginx logs stored in MongoDB:"""
+    client = MongoClient()
+    collection = client.logs.nginx
+
+    doc_count = collection.count_documents({})
+    print('{} logs'.format(doc_count))
+
+    methods_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
-    print("\tmethod GET: {}".format(get_num))
-    print("\tmethod POST: {}".format(post_num))
-    print("\tmethod PUT: {}".format(put_num))
-    print("\tmethod PATCH: {}".format(patch_num))
-    print("\tmethod DELETE: {}".format(delete_num))
-    print("{} status check".format(get_status))
+    for method in methods_list:
+        method_count = collection.count_documents({"method": method})
+        print('\tmethod {}: {}'.format(method, method_count))
+    status_count = collection.count_documents({
+        "method": "GET", "path": "/status"
+    })
+    print('{} status check'.format(status_count))
+
+
+if __name__ == "__main__":
+    nginx_stats_check()
